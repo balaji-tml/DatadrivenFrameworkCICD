917fa1279882c1640dd3fac5336edc35a21e9767
bettertester21@gmail.com <Balaji@gitlab1>
2023-07-19 14:32:36 +0530

    Initial commit with code for a basic framework

---
+++ src/test/java/com/bit/base/BaseTest.java
@@ -0,0 +1,112 @@
+package com.bit.base;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.bit.utilities.ExcelReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+import static com.bit.utilities.TestConfiguration.getCurrentPlatform;
+
+public class BaseTest {
+
+    protected static WebDriver driver;
+    protected Properties config = new Properties();
+    Properties OR = new Properties();
+    FileInputStream fis = null;
+    protected static Logger log;
+//    public ExcelReader excel;
+
+    protected String userDir = System.getProperty("user.dir");
+    public String path = userDir + "/src/test/resources/excel/Testdata.xlsx";
+    public ExcelReader excel = new ExcelReader(path);
+
+    //public ExtentReports rep = ExtentManager.createExtentReports();
+    public ExtentReports extent = null;
+
+    public ExtentTest test = null;
+
+
+    //String browser,url;
+
+    @BeforeSuite
+    public void setUp()
+    {
+        log = LogManager.getLogger(BaseTest.class);
+        log.info("In start of the setUp()");
+        String userDir,browser,url;
+        Platform platform = getCurrentPlatform();
+        log.info("Platform is: "+platform.toString());
+        //extent = ExtentManager.createExtentReports();
+        userDir = System.getProperty("user.dir");
+//        String path = "src/test/resources/excel/Testdata.xlsx";
+        log = LogManager.getLogger(BaseTest.class);
+//        excel = new ExcelReader(path);
+        if(driver == null)
+        {
+            try {
+                fis = new FileInputStream(userDir + "/src/test/resources/properties/Config.properties");
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            try {
+                config.load(fis);
+                log.info("Config file is loaded!");
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            browser = config.getProperty("browser");
+            if(browser.equals("chrome"))
+            {
+                switch (platform) {
+                    case MAC:
+//                        System.setProperty("webdriver.chrome.driver", userDir + "/src/test/resources/executables/chromedriver.exe");
+//                        break;
+                    case WINDOWS:
+                        System.setProperty("webdriver.chrome.driver", userDir + "/src/test/resources/executables/chromedriver.exe");
+                        break;
+                    case LINUX:
+                        System.setProperty("webdriver.chrome.driver", userDir + "/src/test/resources/executables/chromedriver");
+                        break;
+                    default:
+                        System.out.println("Please select valid browser!\n");
+                        break;
+                }
+
+                //System.setProperty("webdriver.chrome.driver", userDir + "/src/test/resources/executables/chromedriver");
+                driver = new ChromeDriver();
+                log.info("Chrome browser is loaded!");
+            }
+            driver.manage().window().maximize();
+        }
+        log.info("In end of the setUp()");
+    }
+
+    @AfterSuite
+    public void tearDown()
+    {
+        log.info("In start of the tearDown()");
+        if(driver !=null)
+        {
+            driver.quit();
+            log.info("Browser is closed!");
+        }
+        log.info("In end of the tearDown()");
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
---
+++ src/test/java/com/bit/swaglabs/ProductsPage.java
@@ -0,0 +1,13 @@
+package com.bit.swaglabs;
+
+import com.bit.base.BasePage;
+import org.openqa.selenium.WebDriver;
+
+public class ProductsPage extends BasePage {
+
+    public ProductsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    
+}
